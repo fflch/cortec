fae2cc0a5dc6a00a060a65b4706fb0bd01775048
Kaue Oliveira Almeida <ueak21@gmail.com>
2018-09-11 16:22:31 -0300

    created crm

---
+++ app/Http/Controllers/CorporaController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Corpora;
+use Illuminate\Http\Request;
+
+class CorporaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Corpora  $corpora
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Corpora $corpora)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Corpora  $corpora
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Corpora $corpora)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Corpora  $corpora
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Corpora $corpora)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Corpora  $corpora
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Corpora $corpora)
+    {
+        //
+    }
+}

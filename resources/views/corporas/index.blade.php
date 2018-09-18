c0727042a243c05f627f213d04c13c38c58b817c
Kaue Oliveira Almeida <ueak21@gmail.com>
2018-09-18 17:28:47 -0300

    ajustes de tema e modificado o redirect de update@Corpora

--- resources/views/corporas/index.blade.php
+++ resources/views/corporas/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
   <div class="container-lista">
     <div class="row">
-      <a class="btn btn-outline-warning" href="/corporas/create" >Criar Corpora</a>
+      <a class="btn btn-success" href="/corporas/create" >Criar Corpora</a>
     </div>
     <div class="row align-items-center row-header-lista px-1 mt-4">
       <div class="col-xs-1 text-center">
@@ -14,20 +14,20 @@
         <ul class="corpora list-group" style='width:100%'>
           @foreach ($corporas as $corpora)
             <li class="corpora list-group-item list-group-item-action">
-              <div class="row">
+              <div class="row align-items-center">
                 <div class="col">
                   <a href="/corporas/{{ $corpora->id }}">{{ $corpora->titulo }}</a>
+                </div>
+                <div class="col">
+                  <form method="GET" action="/corporas/{{ $corpora->id }}/edit">
+                    <button type="submit" class="btn btn-outline-secondary mx-1">Editar</button>
+                  </form>
                 </div>
                 <div class="col">
                   <form class="delete" method="POST" action="/corporas/{{ $corpora->id }}" onsubmit="return confirm('{!! __('messages.confirma') !!}');">
                     {{ csrf_field() }}
                     {{ method_field('delete') }}
-                    <button type="submit" class="btn btn-danger mx-1">Apagar</button>
-                  </form>
-                </div>
-                <div class="col">
-                  <form method="GET" action="/corporas/{{ $corpora->id }}/edit">
-                    <button type="submit" class="btn btn-info mx-1">Editar</button>
+                    <button type="submit" class="btn btn-outline-danger mx-1">Apagar</button>
                   </form>
                 </div>
               </div>

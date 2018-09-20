5dd4731d7c90207af24590f221a409f8579d3371
Kaue Oliveira Almeida <ueak21@gmail.com>
2018-09-20 15:05:00 -0300

    inserido campos no front

--- resources/views/corporas/create.blade.php
+++ resources/views/corporas/create.blade.php
@@ -3,7 +3,8 @@
 @section('content')
   <form method="POST" action="/corporas">
       {{ csrf_field() }}
-      Nome: <input name="titulo">
+      Nome: <input name="titulo" required>
+      Descrição: <input name="descricao" required>
       <button class="btn btn-success" type="submit">Salvar</button>
   </form>
 @endsection
--- resources/views/corporas/edit.blade.php
+++ resources/views/corporas/edit.blade.php
@@ -4,7 +4,8 @@
   <form method="POST" action="/corporas/{{ $corpora->id  }}">
       {{ csrf_field() }}
       {{ method_field('patch') }}
-      Nome: <input name="titulo" value="{{ $corpora->titulo }}">
+      Nome: <input name="titulo" value="{{ $corpora->titulo }}" required>
+      Descrição: <input name="titulo" value="{{ $corpora->descricao }}" required>
       <button class="btn btn-success" type="submit">Salvar</button>
   </form>
 @endsection

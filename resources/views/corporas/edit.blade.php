460784b409886ae1a17855e63919705b360d702e
Kaue Oliveira Almeida <ueak21@gmail.com>
2018-09-25 13:29:29 -0300

    alterado botao add corpus

--- resources/views/corporas/edit.blade.php
+++ resources/views/corporas/edit.blade.php
@@ -1,17 +1,26 @@
 @extends('master')
 
 @section('content')
-  <form method="POST" action="/corporas/{{ $corpora->id  }}">
-      {{ csrf_field() }}
-      {{ method_field('patch') }}
-      <div class="form-group">
-        <label for="titulo">Nome</label>
-        <input type="text" class="form-control" name="titulo" id="titulo" value="{{ $corpora->titulo }}">
-      </div>
-      <div class="form-group">
-        <label for="descricao">Descrição</label>
-        <textarea class="form-control" id="descricao" name="descricao" rows="3">{{ $corpora->descricao }}</textarea>
-      </div>
-      <button type="submit" class="btn btn-success">Salvar</button>
-  </form>
+  <div class="row">
+    <div class="col text-right">
+      <a href="/corporas/{{ $corpora->id }}/corpus/create"  class="btn btn-success">Adicionar Corpus</a>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col">
+      <form method="POST" action="/corporas/{{ $corpora->id  }}">
+        {{ csrf_field() }}
+        {{ method_field('patch') }}
+        <div class="form-group mt-2">
+          <label for="titulo">Nome</label>
+          <input type="text" class="form-control" name="titulo" id="titulo" value="{{ $corpora->titulo }}">
+        </div>
+        <div class="form-group">
+          <label for="descricao">Descrição</label>
+          <textarea class="form-control" id="descricao" name="descricao" rows="3">{{ $corpora->descricao }}</textarea>
+        </div>
+        <button type="submit" class="btn btn-success">Salvar</button>
+      </form>
+    </div>
+  </div>
 @endsection
--- resources/views/corporas/show.blade.php
+++ resources/views/corporas/show.blade.php
@@ -4,5 +4,4 @@
   <h1>{{ $corpora->titulo }}</h1>
   <p>{{ $corpora->descricao }}</p>
   <a href="/corporas/{{ $corpora->id }}/edit"> Editar </a>
-  <a href="/corporas/{{ $corpora->id }}/corpus/create"> Adicionar Corpus </a>
 @endsection

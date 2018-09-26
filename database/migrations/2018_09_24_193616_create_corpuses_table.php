cc8743730d7f184953f996ae233aaf852673049f
Kaue Oliveira Almeida <ueak21@gmail.com>
2018-09-26 15:50:30 -0300

    alterado para longtext o campo de conteudo do corpus

--- database/migrations/2018_09_24_193616_create_corpuses_table.php
+++ database/migrations/2018_09_24_193616_create_corpuses_table.php
@@ -17,7 +17,7 @@ class CreateCorpusesTable extends Migration
             $table->increments('id');
             $table->integer('corpora_id')->unsigned();
             $table->foreign('corpora_id')->references('id')->on('corporas');
-            $table->text('conteudo');
+            $table->longText('conteudo');
             $table->timestamps();
         });
     }

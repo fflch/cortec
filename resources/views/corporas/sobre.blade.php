367ffc1d895872c3386a651216aa8642a4c2d3ba
Kaue Oliveira Almeida <ueak21@gmail.com>
2018-10-01 16:59:53 -0300

    tabela com informações de corpora

--- resources/views/corporas/sobre.blade.php
+++ resources/views/corporas/sobre.blade.php
@@ -27,5 +27,36 @@
     @php
       $i++;
     @endphp
+
+    <div class="row justify-content-md-center">
+      <div class="col-xs-4">
+        <table class="table">
+          <thead>
+            <tr>
+              <th scope="col">{{ $corpora->titulo }}</th>
+              <th scope="col" class="text-center">Inglês</th>
+              <th scope="col" class="text-center">Português</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">Ocorrências/tokens</th>
+              <td class="text-center">{{count($corpora->countTokens())}}</td>
+              <td class="text-center">{{count($corpora->countTokens())}}</td>
+            </tr>
+            <tr>
+              <th scope="row">Formas/types</th>
+              <td class="text-center"></td>
+              <td class="text-center"></td>
+            </tr>
+            <tr>
+              <th scope="row">T/T ratio</th>
+              <td class="text-center"></td>
+              <td class="text-center"></td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
   @endforeach
 @endsection

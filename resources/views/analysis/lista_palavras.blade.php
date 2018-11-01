08151beb3c49f3d56891978c51b1c3f375692fcd
Kaue Oliveira Almeida <ueak21@gmail.com>
2018-11-01 13:28:49 -0300

    ajustando a tabela

--- resources/views/analysis/lista_palavras.blade.php
+++ resources/views/analysis/lista_palavras.blade.php
@@ -4,12 +4,12 @@
   <div class="row">
 
     <div class="col-md-6">
-      <table class="table lista-palavras">
+      <table class="table lista-palavras" id="tbl-lista-palavras">
         <thead>
           <tr>
-            <th scope="col" class="text-center">Pos.</th>
-            <th scope="col" class="text-center">Palavra</th>
-            <th scope="col" class="text-center">Freq.</th>
+            <th scope="col" class="text-center">Pos. <i class="fas fa-sort"></i></th>
+            <th scope="col" class="text-center">Palavra <i class="fas fa-sort"></i></th>
+            <th scope="col" class="text-center">Freq. <i class="fas fa-sort"></i></th>
           </tr>
         </thead>
           @php
@@ -37,17 +37,24 @@
   @parent
   <script>
     const getCellValue = (tr, idx) => tr.children[idx].innerText || tr.children[idx].textContent;
-
     const comparer = (idx, asc) => (a, b) => ((v1, v2) =>
         v1 !== '' && v2 !== '' && !isNaN(v1) && !isNaN(v2) ? v1 - v2 : v1.toString().localeCompare(v2)
         )(getCellValue(asc ? a : b, idx), getCellValue(asc ? b : a, idx));
 
-    // do the work...
     document.querySelectorAll('th').forEach(th => th.addEventListener('click', (() => {
-        const table = th.closest('table').tBodies[0];
+      const table = th.closest('table').tBodies[0];
+      table.parentElement.style.cursor = 'progress';
+      setTimeout(function(){ sortTable(th, table); table.parentElement.style.cursor = ''; }, 400);
+    })));
+
+    function sortTable(th, table){
+      return new Promise(function(resolve, reject) {
         Array.from(table.querySelectorAll('tr:nth-child(n+1)'))
             .sort(comparer(Array.from(th.parentNode.children).indexOf(th), this.asc = !this.asc))
             .forEach(tr => table.appendChild(tr) );
-    })));
+        resolve();
+        });
+    }
+
   </script>
 @endsection

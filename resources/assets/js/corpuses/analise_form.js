b905276c776297b1fdf7b87027745e6824b7bc42
Kaue Oliveira Almeida <ueak21@gmail.com>
2018-11-22 16:17:09 -0200

    fix seleção de corpora multiidioma

--- resources/assets/js/corpuses/analise_form.js
+++ resources/assets/js/corpuses/analise_form.js
@@ -7,6 +7,12 @@ checks_cats.map(function (elm){
   checks_corps.map(function (elm_corp){
     elm_corp.onclick = function (elmcorp){
       checks_corps = Array.from(document.step1.querySelectorAll('[id^="check_'+elm.value+'_"]'));
+
+      //verifica se está visível
+      checks_corps = checks_corps.filter( function( elem, i, array) {
+          return !(elem.offsetParent === null);
+      } );
+
       var checkedCount = document.step1.querySelectorAll('[id^="check_'+elm.value+'_"]:checked').length;
 
       (checkedCount > 0 & checkedCount < checks_corps.length) ? (elm.indeterminate = true) : (elm.indeterminate = false);
@@ -17,7 +23,7 @@ checks_cats.map(function (elm){
   elm.onclick = function () {
     checks_corps = Array.from(document.step1.querySelectorAll('[id^="check_'+elm.value+'_"]'));
     checks_corps.map(function (elm_corp){
-      elm_corp.checked = elm.checked;
+      (elm_corp.offsetParent == null) ? (null) : (elm_corp.checked = elm.checked);
     });
   }
 

272d6815b44575ce569cf7598b51a8088ac4da35
Thiago Gomes Verissimo <thiago.verissimo@usp.br>
2019-01-15 18:44:00 -0200

    Forçar https em produção

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,7 +14,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        // mariadb support
         Schema::defaultStringLength(191);
+        
+        // Forçar https em produção
+        if (env('APP_ENV') === 'production') {
+            \URL::forceScheme('https');
+        } 
     }
 
     /**

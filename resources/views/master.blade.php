d9417f5f0dc33c44d29eaf8bc69c2e8596d31196
Kaue Oliveira Almeida <ueak21@gmail.com>
2019-01-16 16:00:35 -0200

    ajustado o menu superior direito (botão login)

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -4,22 +4,24 @@
     <img src="/img/logo.png" alt="logo" class="align-middle img-fluid">
 @endsection
 
-@section('language')
-    @if (App::isLocale('en'))
-        <a href="/locale/pt_br">Português</a>
-    @else
-        <a href="/locale/en">English</a>
-    @endif
-
-    @auth
-        <form method="post" action="/logout">
-            @csrf
-            <button type="submit">logout</button>
-        </form>
-    @else
-        <a href="/login">Login</a>
-    @endauth
-
+@section('right-top-menu')
+    <div class="col-1 align-self-end text-right">
+        @if (App::isLocale('en'))
+            <a href="/locale/pt_br">Português</a>
+        @else
+            <a href="/locale/en">English</a>
+        @endif
+    </div>
+    <div class="col-1 align-self-end text-center pr-0">
+        @auth
+            <form method="post" action="/logout">
+                @csrf
+                <button class="btn btn-outline-danger" type="submit">logout</button>
+            </form>
+        @else
+            <a href="/login" class="btn btn-outline-success">Login</a>
+        @endauth
+    </div>
 @endsection
 
 @section('menu-itens')

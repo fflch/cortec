a6946fa09bb21e9fb5dbca2c2139d39bd80b89d3
Kaue Oliveira Almeida <ueak21@gmail.com>
2019-01-21 15:12:29 -0200

    relação user-changes

--- app/Change.php
+++ app/Change.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Change extends Model
 {
-    //
+
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
 }
--- app/User.php
+++ app/User.php
@@ -27,4 +27,9 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function changes()
+    {
+        return $this->hasMany('App\Change');
+    }
 }

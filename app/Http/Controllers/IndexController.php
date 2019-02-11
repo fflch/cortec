804d24c20a94b57ba2a782c5c9db127ffe017cec
Kaue Oliveira Almeida <ueak21@gmail.com>
2019-02-11 13:05:26 -0200

    Implementando Ngrams
    Implementando Ngrams.

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -5,6 +5,12 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Corpus;
 use App\Categoria;
+use App\Text;
+
+use App\Utils;
+use TextAnalysis\NGrams\NGramFactory;
+use TextAnalysis\NGrams\Statistic;
+use TextAnalysis\Tokenizers\RegexTokenizer;
 
 class IndexController extends Controller
 {
@@ -15,6 +21,14 @@ class IndexController extends Controller
    */
     public function step1()
     {
+        $text = Text::find(1)->conteudo;
+        $analysis = new Utils($text);
+        $bigrams = NGramFactory::create($analysis->getTokens(), 2);
+        $statistic = new Statistic($bigrams);
+        $stats = $statistic->calculate('rightFisher');
+        arsort($stats);
+        dd($stats);
+
         $categorias = Categoria::whereHas('corpuses.texts')->get();
 
         return view('index', compact('categorias'));

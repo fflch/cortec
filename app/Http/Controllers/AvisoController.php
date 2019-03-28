7e5ca3b6e5742681e4dab6853393de4aff01cb9f
Kaue Oliveira Almeida <ueak21@gmail.com>
2019-03-28 13:10:12 -0300

    inserido model e migration

---
+++ app/Http/Controllers/AvisoController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Aviso;
+use Illuminate\Http\Request;
+
+class AvisoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Aviso  $aviso
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Aviso $aviso)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Aviso  $aviso
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Aviso $aviso)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Aviso  $aviso
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Aviso $aviso)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Aviso  $aviso
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Aviso $aviso)
+    {
+        //
+    }
+}

2a2a194aab3a17b7c819e0f5bc61498f5ee0ef25
Thiago Gomes Verissimo <verissimotgv@gmail.com>
2020-07-09 17:43:34 -0300

    controleer restrict

--- app/Http/Controllers/CategoriaController.php
+++ app/Http/Controllers/CategoriaController.php
@@ -7,6 +7,10 @@ use Illuminate\Http\Request;
 
 class CategoriaController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
 
     /**
      * Show the form for creating a new resource.
--- app/Http/Controllers/ChangeController.php
+++ app/Http/Controllers/ChangeController.php
@@ -7,6 +7,12 @@ use App\Change;
 
 class ChangeController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    
     public function index()
     {
         $changes = Change::orderBy('created_at', 'desc')
